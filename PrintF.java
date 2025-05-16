453e90622578c75cb19401f1c05e73c8160c3890
tanmay-demo <dastanmay260@gmail.com>
2025-05-16 23:42:33 +0530

    printf pro for 5 table

---
+++ PrintF.java
@@ -0,0 +1,18 @@
+public class PrintF {
+    public static void main(String[] args) {
+        int num=5;
+        char new_line='\n';
+        System.out.printf("%d*1=",num );
+        System.out.printf(new_line +"%d*2=",num);
+        System.out.printf(new_line +"%d*3=",num);
+        System.out.printf(new_line +"%d*4=",num);
+        System.out.printf(new_line +"%d*5=",num);
+        System.out.printf(new_line +"%d*6=",num);
+        System.out.printf(new_line +"%d*7=",num);
+        System.out.printf(new_line +"%d*8=",num);
+        System.out.printf(new_line +"%d*9=",num);
+        System.out.printf(new_line +"%d*10=",num);
+        System.out.printf(new_line +"%d*11=",num);
+
+    }
+}
