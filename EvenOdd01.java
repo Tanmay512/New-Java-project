376e6bed70ca1cd6f496f2e857789c48dbba3084
tanmay-demo <dastanmay260@gmail.com>
2025-05-20 22:34:35 +0530

    even odd

---
+++ EvenOdd01.java
@@ -0,0 +1,15 @@
+public class EvenOdd01 {
+    public static void main(String[] args) {
+       int num1 =40;
+       int  num2 =51;
+        if  (num2 % 2 == 0) {
+
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+        }
+
+    }
+
