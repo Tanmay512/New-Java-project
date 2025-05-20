652406335863a6144cb0fdd952e656b9ff0b03fd
tanmay-demo <dastanmay260@gmail.com>
2025-05-20 22:49:29 +0530

    even odd

---
+++ ArithmaticOperators.java
@@ -0,0 +1,12 @@
+public class ArithmaticOperators {
+    public static void main(String[] args) {
+        int x =40;
+        int y=4;
+        float c =2.2f;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(c/2);
+    }
+}
