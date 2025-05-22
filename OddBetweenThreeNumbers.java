fe5ff7fd6a35636410c016dfd1d77d6aace8675e
tanmay-demo <dastanmay260@gmail.com>
2025-05-22 23:27:12 +0530

    Odd 3 number

---
+++ OddBetweenThreeNumbers.java
@@ -0,0 +1,30 @@
+public class OddBetweenThreeNumbers {
+    public static void main(String[] args) {
+        int a =40;
+        int b= 2;
+        int c= 9;
+        if (a%2 ==0) {
+            System.out.println("a is even");
+        }
+            else{
+                System.out.println("odd");
+            }
+        if (b%2 ==0) {
+            System.out.println("b is even");
+        }
+            else{
+                System.out.println("odd");
+            }
+
+        if (c%2 ==0) {
+            System.out.println("c is even");
+        }
+            else{
+                System.out.println("odd");
+            }
+
+
+            }
+        }
+
+
